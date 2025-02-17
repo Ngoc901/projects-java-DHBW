f3d1a7024acdefd19b808a1fe95a7ed1196adfd8
Ngoc901 <huanikii@gmail.com>
2025-02-17 22:25:19 +0100

    binary search

--- src/Algorithms/Algorithms.java
+++ src/Algorithms/Algorithms.java
@@ -52,4 +52,38 @@ public class Algorithms {
         return -1;
     }
 
+    public static void quicksort(int[] arr, int start, int end) {
+
+        if(arr != null && arr.length <= 1) {
+            return;
+        }
+
+        if (start < end) {
+
+            int pivotIndex = partition(arr, start, end);
+            quicksort(arr, start, pivotIndex - 1);
+            quicksort(arr, pivotIndex + 1, end);
+        }
+    }
+
+    public static int partition(int[] arr, int start, int end) {
+
+        int pivot = arr[start];
+        int i = start;
+        for (int j = start + 1; j <= end; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, start, i);
+        return i;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
